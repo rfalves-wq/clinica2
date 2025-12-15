e703c938c22d5f54b5c56042fe94be452d977427
User <rogerioparacambi@gmail.com>
2025-12-15 18:02:48 -0300

    atulizada

--- triagem/models.py
+++ triagem/models.py
@@ -10,4 +10,4 @@ class Triagem(models.Model):
     finalizada = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.paciente.nome} → {self.enfermeiro.nome}'
+        return f'{self.paciente.nome} - {self.enfermeiro.nome}'
